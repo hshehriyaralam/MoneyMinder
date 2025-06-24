b6e6c9646fdaad968e9d10811f3aa55af699dd08
hshehriyaralam <hshehriyaralam@gmail.com>
2025-06-24 18:48:32 +0500

    solve build error

--- src/Components/transactions/IncomeCard.jsx
+++ src/Components/transactions/IncomeCard.jsx
@@ -4,7 +4,7 @@ import CountUp from "../comman/CountUp.jsx";
 import AnimatedContent from '../comman/AnimatedContent.jsx';
 import incomeLogo from "../../assets/images/3588982.png";
 import { useNavigate } from 'react-router-dom'
-import {Context} from '@/context/TransactionContext.jsx';
+import {Context} from '../../Context/TransactionContext.jsx';
 
 const IncomeCard = () => {
   const {incomeAmount} = useContext(Context)
