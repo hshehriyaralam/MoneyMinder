d67070d1004f44ff67fe4292cde187bcbcfb1687
hshehriyaralam <hshehriyaralam@gmail.com>
2025-06-24 22:16:25 +0500

    decease width

--- src/Components/comman/Ainimate-LOgin.jsx
+++ src/Components/comman/Ainimate-LOgin.jsx
@@ -2,7 +2,7 @@ import animationData from "../../Context/animate-Login.json"
 import Lottie from "lottie-react";
 
 const  AnimateLogin = () => {
-  return <Lottie animationData={animationData} loop={true} className="w-96 " />;
+  return <Lottie animationData={animationData} loop={true} className="w-80" />;
 };
 
 export default AnimateLogin ;
--- src/Components/comman/AnimateSignUp.jsx
+++ src/Components/comman/AnimateSignUp.jsx
@@ -2,7 +2,7 @@ import animationData from "../../Context/animation-SignUp.json"
 import Lottie from "lottie-react";
 
 const  AnimateSignUp = () => {
-  return <Lottie animationData={animationData} loop={true} className="w-96 " />;
+  return <Lottie animationData={animationData} loop={true} className="w-80" />;
 };
 
 export default AnimateSignUp ;
